b2092076c8b1f4ce8a01cbc904d257aa16012976
madelin <madelintania@gmail.com>
2022-05-29 22:50:21 -0300

    Formulario final

--- src/form.js
+++ src/form.js
@@ -6,10 +6,17 @@ form.addEventListener("submit", onsubmit);
 
 function onsubmit(event) {
     event.preventDefault()
-    console.log(form.elements["nombre"].value);
-    mailWindow.setAttribute("href", "mailto:madelintania@gmail.com?subject=" + form.elements["nombre"].value + "&body=test")
+    let mailto = "mailto:madelintania@gmail.com"
+    let asunto = form.elements["asunto"].value;
+    let message = form.elements["mensaje"].value;
+    mailWindow.setAttribute("href", `${mailto}?subject=${asunto}&body=${message}`)
     mailWindow.click()
 
 }
 
 
+
+
+/* 1 - crear funcion para validar cada campo, una vez hecha la validación
+ con resultado true o false.
+2 - Utilizar esa funcion para determinar el envio del formulario */
